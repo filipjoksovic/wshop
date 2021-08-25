cbcbac196b4a78102b2b58d7d24624bf8c8bf010
= <=>
2021-08-25 20:08:30 +0200

    Product edit and delete

--- pocetna.php
+++ pocetna.php
@@ -11,109 +11,115 @@
 
 <body>
     <?php include("navigation.php"); ?>
-    <?php var_dump($_SESSION); ?>
+    <!-- <?php var_dump($_SESSION); ?> -->
     <?php if (isset($_SESSION['error'])) : ?>
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            <span class="sr-only">Close</span>
-        </button>
-        <strong>Greska!</strong> <?php echo $_SESSION['error'];
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                <span class="sr-only">Close</span>
+            </button>
+            <strong>Greska!</strong> <?php echo $_SESSION['error'];
                                         unset($_SESSION["error"]); ?>.
-    </div>
+        </div>
     <?php endif; ?>
     <?php if (isset($_SESSION['message'])) : ?>
-    <div class="alert alert-primary alert-dismissible fade show" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            <span class="sr-only">Close</span>
-        </button>
-        <strong>Uspeh!</strong> <?php echo $_SESSION['message'];
+        <div class="alert alert-primary alert-dismissible fade show" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                <span class="sr-only">Close</span>
+            </button>
+            <strong>Uspeh!</strong> <?php echo $_SESSION['message'];
                                     unset($_SESSION["message"]); ?>.
-    </div>
+        </div>
     <?php endif; ?>
     <?php if ($_SESSION['user']['type'] == "kupac") : ?>
-    <h3>Kupac pocetna</h3>
+        <h3>Kupac pocetna</h3>
     <?php elseif ($_SESSION['user']['type'] == "prodavac") : ?>
-    <?php
+        <?php
         require "db.php";
-        $query = "SELECT * FROM products WHERE seller = '{$_SESSION['user']['username']}'";
+        $query = "SELECT products.*, product_images.path FROM products INNER JOIN product_images on products.id = product_images.product_id WHERE seller = '{$_SESSION['user']['username']}' GROUP BY products.name";
         $products = $database->query($query)->fetch_all(MYSQLI_ASSOC);
+        $query = "SELECT DISTINCT category from products";
+        $categories = $database->query($query)->fetch_all();
         ?>
-    <div class="p-5 row">
-        <div class="col-md-9">
-            <h3 class="text-center">
-                Pregled Vasih proizvoda
-            </h3>
-            <?php if (count($products) == 0) : ?>
-            <h5 class="text-center">Nemate dodat ni jedan proizvod.</h5>
-            <?php else : ?>
-            <div class="table-responsive">
-                <table class="table ">
-                    <th>Slika</th>
-                    <th>Sifra</th>
-                    <th>Naziv</th>
-                    <th>Kategorije</th>
-                    <th>Cena</th>
-                    <th>Obrisi proizvod</th>
-                    <th>Izmeni proizvod</th>
-                    <tbody>
-                        <?php foreach ($products as $product) : ?>
-                        <?php var_dump($product); ?>
-                        <tr>
-                            <td><?php echo $product['id']; ?></td>
-                            <td><?php echo $product['id']; ?></td>
-                            <td><?php echo $product['name']; ?></td>
-                            <td><?php echo $product['category']; ?></td>
-                            <td><?php echo $product['price']; ?></td>
-                            <td><button class="btn btn-danger">Ukloni proizvod</button></td>
-                            <td><button class="btn btn-warning">Ukloni proizvod</button></td>
-                        </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-            </div>
-            <?php endif; ?>
-        </div>
-        <div class="col-md-3">
-            <h3 class="text-center">Dodaj proizvod</h3>
-            <form method="POST" action="server.php" enctype="multipart/form-data">
-                <div class="form-group">
-                    <label for="productName">Naziv proizvoda</label>
-                    <input type="text" class="form-control" id="productName" name="pname">
-                </div>
-                <div class="form-group">
-                    <label for="exampleFormControlSelect1">Kategorija proizvoda</label>
-                    <select class="form-control" name="cat" id="exampleFormControlSelect1">
-                        <option value=-1>Samostalan unos kategorije</option>
-                    </select>
-                </div>
-                <div class="form-group" id="catManual">
-                    <label for="productName">Kategorija proizvoda</label>
-                    <input type="text" class="form-control" id="productName" name="catManual">
-                </div>
-                <div class="form-group">
-                    <label for="productPrice">Cena proizvoda</label>
-                    <input type="text" class="form-control" id="productPrice" name="price">
-                </div>
-                <div class="input-group mb-3">
-                    <div class="custom-file">
-                        <input type="file" name="images[]" class="custom-file-input" id="inputGroupFile01" multiple>
-                        <label class="custom-file-label" for="inputGroupFile01">Odaberite slike</label>
+        <div class="p-5 container-fluid row">
+            <div class="col-md-9">
+                <h3 class="text-center">
+                    Pregled Vasih proizvoda
+                </h3>
+                <?php if (count($products) == 0) : ?>
+                    <h5 class="text-center">Nemate dodat ni jedan proizvod.</h5>
+                <?php else : ?>
+                    <div class="table-responsive">
+                        <table class="table ">
+                            <th>Slika</th>
+                            <th>Sifra</th>
+                            <th>Naziv</th>
+                            <th>Kategorija</th>
+                            <th>Cena</th>
+                            <th>Obrisi proizvod</th>
+                            <th>Izmeni proizvod</th>
+                            <tbody>
+                                <?php foreach ($products as $product) : ?>
+                                    <!-- <?php var_dump($product); ?> -->
+                                    <tr>
+                                        <td><img src="<?php echo $product['path']; ?>" class="img-thumbnail pimg"></td>
+                                        <td class="align-middle"><?php echo $product['id']; ?></td>
+                                        <td class="align-middle"><?php echo $product['name']; ?></td>
+                                        <td class="align-middle"><?php echo ucfirst($product['category']); ?></td>
+                                        
+                                        <td class="align-middle"><?php echo $product['price']; ?></td>
+                                        <td class="align-middle"><a href = "removeproduct.php?id=<?php echo $product['id'];?>" class="btn btn-danger">Ukloni proizvod</a></td>
+                                        <td class="align-middle"><a href = "editproduct.php?id=<?php echo $product['id'];?>" class="btn btn-warning">Izmeni proizvod</a></td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
                     </div>
-                </div>
-                <input type="hidden" name="username" value="<?php echo $_SESSION['user']['username']; ?>">
-                <input type="hidden" name="add_product" value="1">
-                <button type="submit" class="btn btn-primary btn-block">Dodaj proizvod</button>
-            </form>
-        </div>
+                <?php endif; ?>
+            </div>
+            <div class="col-md-3">
+                <h3 class="text-center">Dodaj proizvod</h3>
+                <form method="POST" action="server.php" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="productName">Naziv proizvoda</label>
+                        <input type="text" class="form-control" id="productName" name="pname">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlSelect1">Kategorija proizvoda</label>
+                        <select class="form-control" name="cat" id="exampleFormControlSelect1">
+                            <?php foreach($categories as $category):?>
+                                <option value = "<?php echo $category[0];?>"><?php echo ucfirst($category[0]);?></option>
+                                <?php endforeach;?>
+                                <option value=-1>Samostalan unos kategorije</option>
+                            </select>
+                    </div>
+                    <div class="form-group" id="catManual">
+                        <label for="productName">Kategorija proizvoda</label>
+                        <input type="text" class="form-control" id="productName" name="catManual">
+                    </div>
+                    <div class="form-group">
+                        <label for="productPrice">Cena proizvoda</label>
+                        <input type="text" class="form-control" id="productPrice" name="price">
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="custom-file">
+                            <input type="file" name="images[]" class="custom-file-input" id="inputGroupFile01" multiple>
+                            <label class="custom-file-label" for="inputGroupFile01">Odaberite slike</label>
+                        </div>
+                    </div>
+                    <input type="hidden" name="username" value="<?php echo $_SESSION['user']['username']; ?>">
+                    <input type="hidden" name="add_product" value="1">
+                    <button type="submit" class="btn btn-primary btn-block">Dodaj proizvod</button>
+                </form>
+            </div>
 
-    </div>
+        </div>
     <?php elseif ($_SESSION['user']['type'] == "admin") : ?>
-    <h3>Admin pocetna</h3>
+        <h3>Admin pocetna</h3>
 
     <?php else : ?>
-    <h3>Gost pocetna</h3>
+        <h3>Gost pocetna</h3>
     <?php endif; ?>
 </body>
 
--- server.php
+++ server.php
@@ -54,8 +54,9 @@
             $category = $_POST['catManual'];
         }
         else{
-            $category = strtolower($_POST['cat']);
+            $category = $_POST['cat'];
         }
+        $category = strtolower($category);
         $query = "INSERT INTO products (name,price,category,seller) VALUES ('${product_name}',{$price},'{$category}','{$username}')";
         $index = 0;
         if($database->query($query) === TRUE){
@@ -86,6 +87,32 @@
             $_SESSION['error'] = $query;
             // $_SESSION['error'] = "Greska prilikom dodavanja proizvoda u bazu podataka. Tekst greske: " . $database->error;
             header("location: pocetna.php");
+            return;
+        }
+    }
+    if(isset($_POST['edit_product'])){
+        $product_name = $_POST["pname"];
+        $price = $_POST['price'];
+        $username = $_POST['username'];
+        $product_id = $_POST['product_id'];
+        $category;
+        if ($_POST['cat'] == -1) {
+            $category = $_POST['catManual'];
+        } else {
+            $category = $_POST['cat'];
+        }
+        $category = strtolower($category);
+
+        $query = "UPDATE products SET name = '{$product_name}',price = '{$price}', category = '{$category}' WHERE id = {$product_id}";
+
+        if($database->query($query) === TRUE){
+            $_SESSION['message'] = "Uspesno izmenjen proizvod";
+            header("location:pocetna.php");
+            return;
+        }
+        else{
+            $_SESSION['error'] = "Greska prilikom izmene proizvoda";
+            header("location:pocetna.php");
             return;
         }
     }
