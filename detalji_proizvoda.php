7758a3cb01ae558d3eeaa4b1d0fb62592c654406
= <=>
2021-08-26 17:14:53 +0200

    Admin scaffolding

--- detalji_proizvoda.php
+++ detalji_proizvoda.php
@@ -63,77 +63,83 @@
                 <span class="sr-only">Next</span>
             </a>
         </div>
-        <div class="details mt-4">
-            <h3 class="text-center"><?php echo $product['name']; ?></h3>
-            <h5 class="text-center"><?php echo $product['seller']; ?></h5>
-            <div class="review text-center">
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star-o"></i>
-                <i class="fa fa-star-o"></i>
+        <div class="details mt-4 row">
+            <div class="col-md-7">
+                <h3 class="text-center"><?php echo $product['name']; ?></h3>
+                <h5 class="text-center"><?php echo $product['seller']; ?></h5>
+                <div class="review text-center">
+                    <i class="fa fa-star text-primary"></i>
+                    <i class="fa fa-star text-primary"></i>
+                    <i class="fa fa-star text-primary"></i>
+                    <i class="fa fa-star-o text-primary"></i>
+                    <i class="fa fa-star-o text-primary"></i>
+                </div>
             </div>
-            <h5 class="text-center text-primary"><?php echo number_format($product['price'], 2) . "din"; ?></h5>
-            <div class="reviews mt-5">
-                <ul class="nav nav-pills" id="myTab" role="tablist">
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Opis</a>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#reviews" role="tab" aria-controls="profile" aria-selected="false">Recenzije proizvoda</a>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <a class="nav-link" id="contact-tab" data-toggle="tab" href="#personal_review" role="tab" aria-controls="contact" aria-selected="false">Licna recenzija</a>
-                    </li>
-                </ul>
-                <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane fade show active" id="description" role="tabpanel" aria-labelledby="home-tab">
-                        <p>Opis proizvoda:</p>
-                        <span><?php echo $product['description']; ?></span>
-                    </div>
-                    <div class="tab-pane fade" id="reviews" role="tabpanel" aria-labelledby="profile-tab">...</div>
-                    <div class="tab-pane fade container w-50 my-3" id="personal_review" role="tabpanel" aria-labelledby="contact-tab">
-                        <?php
-                        $query = "SELECT * FROM product_reviews WHERE product_id = {$product['id']} AND username = '{$_SESSION['user']['username']}'";
-                        $review = $database->query($query)->fetch_assoc();
-                        ?>
-                        <?php if ($review == null) : ?>
-                            <form action="server.php" method="POST">
-                                <div class="form-group">
-                                    <label for="grade">Ocena proizvoda(1-5)</label>
-                                    <input type="number" min="1" max="5" name="grade" id="grade" class="form-control" placeholder="" aria-describedby="helpId">
-                                </div>
-                                <div class="form-group">
-                                    <label for="review">Recenzija</label>
-                                    <textarea type="text" name="review" id="review" class="form-control" placeholder="" aria-describedby="helpId"></textarea>
-                                </div>
-                                <input type="hidden" name="product_id" value="<?php echo $product['id']; ?>">
-                                <input type="hidden" name="add_review" value="1">
-                                <input type="hidden" name="username" value="<?php echo $_SESSION['user']['username']; ?>">
-                                <button type="submit" class="btn btn-success btn-block">Oceni proizvod</button>
+            <div class="col-md-5 d-flex align-items-center justify-content-center flex-column">
+                <h5 class="text-center text-primary"><?php echo number_format($product['price'], 2) . "din"; ?></h5>
+                <button class="btn btn-primary">Dodaj u korpu</button>
+            </div>
+        </div>
+        <div class="reviews mt-5">
+            <ul class="nav nav-pills" id="myTab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Opis</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#reviews" role="tab" aria-controls="profile" aria-selected="false">Recenzije proizvoda</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="contact-tab" data-toggle="tab" href="#personal_review" role="tab" aria-controls="contact" aria-selected="false">Licna recenzija</a>
+                </li>
+            </ul>
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="description" role="tabpanel" aria-labelledby="home-tab">
+                    <p>Opis proizvoda:</p>
+                    <span><?php echo $product['description']; ?></span>
+                </div>
+                <div class="tab-pane fade" id="reviews" role="tabpanel" aria-labelledby="profile-tab">...</div>
+                <div class="tab-pane fade container w-50 my-3" id="personal_review" role="tabpanel" aria-labelledby="contact-tab">
+                    <?php
+                    $query = "SELECT * FROM product_reviews WHERE product_id = {$product['id']} AND username = '{$_SESSION['user']['username']}'";
+                    $review = $database->query($query)->fetch_assoc();
+                    ?>
+                    <?php if ($review == null) : ?>
+                        <form action="server.php" method="POST">
+                            <div class="form-group">
+                                <label for="grade">Ocena proizvoda(1-5)</label>
+                                <input type="number" min="1" max="5" name="grade" id="grade" class="form-control" placeholder="" aria-describedby="helpId">
+                            </div>
+                            <div class="form-group">
+                                <label for="review">Recenzija</label>
+                                <textarea type="text" name="review" id="review" class="form-control" placeholder="" aria-describedby="helpId"></textarea>
+                            </div>
+                            <input type="hidden" name="product_id" value="<?php echo $product['id']; ?>">
+                            <input type="hidden" name="add_review" value="1">
+                            <input type="hidden" name="username" value="<?php echo $_SESSION['user']['username']; ?>">
+                            <button type="submit" class="btn btn-success btn-block">Oceni proizvod</button>
 
-                            </form>
-                        <?php else : ?>
-                            <form action="server.php" method="POST">
-                                <div class="form-group">
-                                    <label for="grade">Ocena proizvoda(1-5)</label>
-                                    <input type="number" min="1" max="5" name="grade" id="grade" class="form-control" value="<?php echo $review['grade']; ?>" placeholder="" aria-describedby="helpId">
-                                </div>
-                                <div class="form-group">
-                                    <label for="review">Recenzija</label>
-                                    <textarea type="text" name="review" id="review" class="form-control" placeholder="" aria-describedby="helpId" ><?php echo $review['review']; ?></textarea>
-                                </div>
-                                <input type="hidden" name="product_id" value="<?php echo $product['id']; ?>">
-                                <input type="hidden" name="edit_review" value="1">
-                                <input type="hidden" name="username" value="<?php echo $_SESSION['user']['username']; ?>">
-                                <button type="submit" class="btn btn-danger btn-block">Izmeni ocenu</button>
+                        </form>
+                    <?php else : ?>
+                        <form action="server.php" method="POST">
+                            <div class="form-group">
+                                <label for="grade">Ocena proizvoda(1-5)</label>
+                                <input type="number" min="1" max="5" name="grade" id="grade" class="form-control" value="<?php echo $review['grade']; ?>" placeholder="" aria-describedby="helpId">
+                            </div>
+                            <div class="form-group">
+                                <label for="review">Recenzija</label>
+                                <textarea type="text" name="review" id="review" class="form-control" placeholder="" aria-describedby="helpId"><?php echo $review['review']; ?></textarea>
+                            </div>
+                            <input type="hidden" name="product_id" value="<?php echo $product['id']; ?>">
+                            <input type="hidden" name="edit_review" value="1">
+                            <input type="hidden" name="username" value="<?php echo $_SESSION['user']['username']; ?>">
+                            <button type="submit" class="btn btn-danger btn-block">Izmeni ocenu</button>
 
-                            </form>
-                        <?php endif; ?>
-                    </div>
+                        </form>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
+    </div>
     </div>
     <script>
         $(function() {
