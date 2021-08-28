5bb2895c3f0ded7bf4d1ae2fa177c02255e3866e
= <=>
2021-08-28 21:26:59 +0200

    Database Update

--- wshop.sql
+++ wshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 28, 2021 at 11:04 AM
+-- Generation Time: Aug 28, 2021 at 09:26 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -42,9 +42,8 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `description`, `price`, `stock`, `category`, `seller`) VALUES
-(4, 'Haljina', NULL, 8000, 2, 'odeca', 'petarmarkovic'),
-(5, 'Converse ranac', NULL, 3500, 1, 'ranac', 'petarmarkovic'),
-(9, 'Test proizvod', 'Test', 4000, 0, 'test', 'petarmarkovic'),
+(4, 'Haljina', NULL, 8000, 1, 'odeca', 'petarmarkovic'),
+(5, 'Converse ranac', 'Converse ranac za smestaj Vama najpotrebnijih svakodnevnih stvari.', 3500, 3, 'ranac', 'petarmarkovic'),
 (10, 'Ugaona garnitura GALIJA LUX M ', 'Ugaona garnitura, kao sto se moze videti na slici', 40000, 1, 'namestaj', 'jadrankajadic'),
 (11, 'Regal PRATO ', 'Odlican regal visokog kvaliteta za Vas dom.\r\nNapomena: Uz regal ne dolazi televizor, biljke i ostali dodaci.', 28490, 9, 'namestaj', 'jadrankajadic'),
 (12, 'Fotelja NEW YORK ', 'Fotelja New York model, odlican dodatak svakoj dnevnoj sobi.', 29000, 3, 'namestaj', 'jadrankajadic'),
@@ -71,7 +70,6 @@ INSERT INTO `product_images` (`product_id`, `path`) VALUES
 (4, './media/14-60-fe81b47cK042_ZIELONY.webp'),
 (5, './media/1-f2-e475391d71027403_xxl.webp'),
 (5, './media/71027403_xxl_a1.webp'),
-(9, './media/14-4d-f6eefba7k042_green_1L.webp'),
 (10, './media/21028866.jpg'),
 (10, './media/csm_21028866_b0f3e53a6b.jpg'),
 (11, './media/11008777.jpg'),
@@ -109,12 +107,13 @@ CREATE TABLE `product_orders` (
 --
 
 INSERT INTO `product_orders` (`id`, `product_id`, `quantity`, `username`, `first_name`, `last_name`, `country`, `city`, `address`, `zip_code`, `payment_option`, `order_date`, `status`) VALUES
-(1, 4, 3, 'filipjoksovic', 'Filip', 'Joksovic', 'Srbija', 'Kragujevac', 'Episkopa Save 17', '31000', 0, '2021-08-26 01:05:51', 0),
-(2, 5, 1, 'filipjoksovic', 'Filip', 'Joksovic', 'Srbija', 'Kragujevac', 'Episkopa Save 17', '31000', 0, '2021-08-26 01:05:51', 1),
-(3, 5, 2, 'filipjoksovic', 'Filip', 'Joksovic', 'Srbija', 'Kragujevac', 'Episkopa Save 17', '31000', 0, '2021-08-26 03:19:57', 0),
-(4, 4, 1, 'filipjoksovic', 'Filip', 'Joksovic', 'Srbija', 'Kragujevac', 'Episkopa Save 17', '31000', 0, '2021-08-26 03:19:57', 0),
-(6, 13, 1, 'jovicastevic', 'Jovica', 'Stevic', 'Srbija', 'Cacak', 'Kneza Lazara 2', '32210', 0, '2021-08-27 23:52:46', 0),
-(7, 11, 1, 'jovicastevic', 'Petar', 'Jovanovic', 'Srbija', 'Valjevo', 'Karadjorjeva 3', '30000', 1, '2021-08-27 23:53:34', 1);
+(6, 13, 1, 'jovicastevic', 'Jovica', 'Stevic', 'Srbija', 'Cacak', 'Kneza Lazara 2', '32210', 0, '2021-08-27 23:52:46', 1),
+(7, 11, 1, 'jovicastevic', 'Petar', 'Jovanovic', 'Srbija', 'Valjevo', 'Karadjorjeva 3', '30000', 1, '2021-08-27 23:53:34', 1),
+(8, 5, 1, 'pericamikic', 'Perica', 'Mikic', 'Srbija', 'Novi Sad', 'Kralja Aleksandra 12', '27000', 0, '2021-08-28 13:11:53', 1),
+(9, 4, 1, 'pericamikic', 'Perica', 'Mikic', 'Srbija', 'Novi Sad', 'Kralja Aleksandra 12', '27000', 0, '2021-08-28 13:11:53', 1),
+(11, 5, 1, 'milanmiljkovic', 'Aleksandar', 'Markovic', 'Srbija', 'Valjevo', 'Jovana Sremca', '4500', 1, '2021-08-28 13:13:33', 1),
+(12, 4, 1, 'milanmiljkovic', 'Aleksandar', 'Markovic', 'Srbija', 'Valjevo', 'Jovana Sremca', '4500', 1, '2021-08-28 13:13:33', 1),
+(13, 12, 1, 'milanmiljkovic', 'Aleksandar', 'Markovic', 'Srbija', 'Valjevo', 'Jovana Sremca', '4500', 1, '2021-08-28 13:13:33', 1);
 
 -- --------------------------------------------------------
 
@@ -123,6 +122,7 @@ INSERT INTO `product_orders` (`id`, `product_id`, `quantity`, `username`, `first
 --
 
 CREATE TABLE `product_reviews` (
+  `id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `grade` int(11) NOT NULL,
@@ -134,8 +134,14 @@ CREATE TABLE `product_reviews` (
 -- Dumping data for table `product_reviews`
 --
 
-INSERT INTO `product_reviews` (`product_id`, `username`, `grade`, `review`, `review_date`) VALUES
-(4, 'filipjoksovic', 3, 'Meh', '2021-08-26 04:19:27');
+INSERT INTO `product_reviews` (`id`, `product_id`, `username`, `grade`, `review`, `review_date`) VALUES
+(2, 13, 'jovicastevic', 5, 'Sjajne slusalice za pristupacnu cenu', '2021-08-28 13:10:37'),
+(3, 11, 'jovicastevic', 3, 'Kvalitet moze biti bolji', '2021-08-28 13:10:51'),
+(4, 5, 'pericamikic', 4, 'Sjajan rancic za svaki dan, mada kvalitet savova moze biti boji', '2021-08-28 13:15:08'),
+(5, 4, 'pericamikic', 5, 'Udobna haljina', '2021-08-28 13:15:28'),
+(6, 5, 'milanmiljkovic', 3, 'Kupio sam ga jer su ga svi hvalili, nista posebno', '2021-08-28 13:16:17'),
+(7, 12, 'milanmiljkovic', 5, 'JAKO udobna fotelja', '2021-08-28 13:16:28'),
+(8, 4, 'milanmiljkovic', 5, 'Preudobno', '2021-08-28 13:16:38');
 
 -- --------------------------------------------------------
 
@@ -160,7 +166,6 @@ INSERT INTO `users` (`username`, `first_name`, `last_name`, `email`, `password`,
 ('admin', 'Admin', 'Admin', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
 ('aleksandrapetrovic', 'Aleksandra', 'Petrovic', 'aleksandrapetrovic@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'prodavac'),
 ('filipfilipovic', 'Filip', 'Filipovic', 'filipfilipovic@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'prodavac'),
-('filipjoksovic', 'Filip', 'Joksovic', 'test@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'kupac'),
 ('jadrankajadic', 'Jadranka', 'Jadic', 'jadrankajadic@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'prodavac'),
 ('jelenamilenkovic', 'Jelena', 'Milenkovic', 'jelenamilenkovic@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'prodavac'),
 ('jovicastevic', 'Jovica', 'Stevic', 'jovicastevic@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'kupac'),
@@ -199,6 +204,7 @@ ALTER TABLE `product_orders`
 -- Indexes for table `product_reviews`
 --
 ALTER TABLE `product_reviews`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `product_id` (`product_id`),
   ADD KEY `username` (`username`);
 
@@ -223,7 +229,13 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `product_orders`
 --
 ALTER TABLE `product_orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `product_reviews`
+--
+ALTER TABLE `product_reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
